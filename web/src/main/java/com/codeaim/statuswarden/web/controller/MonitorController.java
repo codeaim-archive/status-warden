520e8c333f35edc916d4126c18e26494497bb0d5
Gerard Downes <gdownes@gmail.com>
2015-10-03 14:29:39 +0100

    Added api usage get monitors example

--- web/src/main/java/com/codeaim/statuswarden/web/controller/MonitorController.java
+++ web/src/main/java/com/codeaim/statuswarden/web/controller/MonitorController.java
@@ -1,16 +1,35 @@
 package com.codeaim.statuswarden.web.controller;
 
+import com.codeaim.statuswarden.common.model.Monitor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MonitorController
 {
+    @Value("${com.codeaim.statuswarden.web.api.url}")
+    private String apiUrl;
+
     @RequestMapping("/monitor")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String monitorList(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model)
+    {
+        model.addAttribute("monitors", getMonitors());
         return "home";
     }
+
+    private Monitor[] getMonitors()
+    {
+        try
+        {
+            return new RestTemplate().getForObject(apiUrl, Monitor[].class);
+        } catch (Exception exception)
+        {
+            return new Monitor[0];
+        }
+    }
+
 }
